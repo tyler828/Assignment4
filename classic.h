513a5213cda9657208f8cb3ee11babff090b7e4f
Loer9999 <105740154+Loer9999@users.noreply.github.com>
2022-05-23 11:50:16 -0700

    Update classic.h

--- classic.h
+++ classic.h
@@ -5,26 +5,27 @@
 
 // -------------------------------- classic.h ----------------------------------
 // Programmer Name: Team C--     Course Section Number: CSS 343 B
-// Creation Date: 5/19/2022
+// Creation Date: 5/23/2022
 // Date of Last Modification:
 //
 // Purpose: This file is the header file of the classic class, a subclass of the
 // movie class. The classic class overwrites the comparison operators so that
-// the comparison of two classic movies is based on their titles, then the years
-// they were released.
+// the comparison of two classic movies is based on the dates when they were
+// released and their major actors.
 // ----------------------------------------------------------------------------
 
 class Classic : public Movie
 {
 public:
-    Classic(std::string title, std::string director, std::string releaseYear, int max);
+    Classic(std::string title, std::string director, std::string releaseYear, std::string releaseMonth, std::string actorName, int max);
 
     bool operator<(Classic& rhs) const;
 
     bool operator>(Classic& rhs) const;
+    
+    friend std::ostream& operator<<(std::ostream& output, const Classic& rhs);
 private:
     std:: string actorName;
     
     std:: string releaseMonth;
-
 };
