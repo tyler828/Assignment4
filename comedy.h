03d282bce22fd5730c95f74e1414bab99c0a5336
Loer9999 <105740154+Loer9999@users.noreply.github.com>
2022-05-22 21:28:17 -0700

    Update comedy.h

--- comedy.h
+++ comedy.h
@@ -17,57 +17,10 @@
 class Comedy : public Movie
 {
 public:
-    // --------------------------Parametrized Constructor--------------------------
-    // Description: The parametrized constructor creates a comedy movie object
-    // based on the given title, director, release year, and maximum stock.
-    //
-    // Pre: The given information must correctly represent the comedy movie.
-    //
-    // Post: A comedy movie object that has the given title, director, release
-    // year, and maximum stock exists. The current stock is initialized to the
-    // maximum stock.
-    //
-    // Param: title, the title of the comedy movie.
-    //
-    // Param: director, the director of the comedy movie.
-    //
-    // Param: releaseYear, the year when the comedy movie was released.
-    //
-    // Param: max, the maximum stock of the comedy movie.
     Comedy(std::string title, std::string director, std::string releaseYear, int max);
 
-    // ---------------------------------operator<----------------------------------
-    // Description: The method operator< overloads the operator <, comparing this
-    // comedy movie, which is on the left-hand side of the operator <, with the
-    // comedy movie object on the right-hand side based on their titles, then the
-    // years they were released.
-    //
-    // Post: These two comedy movie objects do not change.
-    //
-    // Param: rhs, which is the comedy movie object on the right-hand side.
-    //
-    // Return: This method returns true if the title of this comedy movie comes
-    // before the title of the comedy movie on the right-hand side with an
-    // alphabetical order. This method also turns true if these two comedy movies
-    // have the same title but this comedy movie was released before the other
-    // one. This method returns false otherwise.
     bool operator<(Comedy& rhs) const;
 
-    // ---------------------------------operator>----------------------------------
-    // Description: The method operator> overloads the operator >, comparing this
-    // comedy movie, which is on the left-hand side of the operator >, with the
-    // comedy movie object on the right-hand side based on their titles, then the
-    // years they were released.
-    //
-    // Post: These two comedy movie objects do not change.
-    //
-    // Param: rhs, which is the comedy movie object on the right-hand side.
-    //
-    // Return: This method returns true if the title of this comedy movie comes
-    // after the title of the comedy movie on the right-hand side with an
-    // alphabetical order. This method also turns true if these two comedy movies
-    // have the same title but this comedy movie was released after the other
-    // one. This method returns false otherwise.
     bool operator>(Comedy& rhs) const;
 private:
 
