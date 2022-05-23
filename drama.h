549b76eb12d7c1012bc4005c889a04056f8ac54d
Loer9999 <105740154+Loer9999@users.noreply.github.com>
2022-05-23 11:47:17 -0700

    Update drama.h

--- drama.h
+++ drama.h
@@ -5,13 +5,13 @@
 
 // -------------------------------- drama.h ----------------------------------
 // Programmer Name: Team C--     Course Section Number: CSS 343 B
-// Creation Date: 5/19/2022
+// Creation Date: 5/23/2022
 // Date of Last Modification:
 //
 // Purpose: This file is the header file of the drama class, a subclass of the
 // movie class. The drama class overwrites the comparison operators so that
-// the comparison of two drama movies is based on their titles, then the years
-// they were released.
+// the comparison of two drama movies is based on their directors, and then
+// their titles.
 // ----------------------------------------------------------------------------
 
 class Drama : public Movie
@@ -22,6 +22,8 @@ public:
     bool operator<(Drama& rhs) const;
 
     bool operator>(Drama& rhs) const;
+    
+    friend std::ostream& operator<<(std::ostream& output, const Drama& rhs);
 private:
 
 };
