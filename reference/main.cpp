a4b81d11bab35f155d82270f53e7fbdda93d3bd8
Loer999 <polintu@uw.edu>
2022-05-28 12:03:13 -0700

    Updated the operator[].

--- reference/main.cpp
+++ reference/main.cpp
@@ -1,13 +1,20 @@
-#include <string>
+#include <iostream>
 #include "table.h"
 
 int main()
 {
-    MyTable<int, char> test;
-    test.insert(143, 'A');
-    test.insert(43, 'B');
-    test.insert(243, 'C');
-    test.insert(3, 'D');
-    test.insert(2, 'E');
+    MyTable<char, float> test;
+    test.insert('5', 0.08);
+    test.insert('3', 0.03);
+    test.insert('5', 0.05);
+    test.insert('h', 6.5187);
+    test.insert('H', 561.01);
+    test.insert('#', 333.333);
+    std::cout << test['5'] << std::endl;
+    std::cout << test['3'] << std::endl;
+    std::cout << test['h'] << std::endl;
+    std::cout << test['H'] << std::endl;
+    std::cout << test['#'] << std::endl;
+    // std::cout << test['%'] << std::endl;
     return 0;
 }
