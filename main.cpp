b63e84028249490b442c0cf49362726fb6315c25
Tyler Schmale <tschmale@uw.edu>
2022-06-01 08:58:16 -0700

    added main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "rental_store.h"
+#include <fstream>
+
+
+int main(){
+    std::ifstream commands("data4commands.txt");
+    std::ifstream customers("data4customers.txt");
+    std::ifstream movies("data4movies.txt");
+
+    RentalStore Blockbuster;
+
+    Blockbuster.initializeInventory(commands);
+    Blockbuster.addCustomers(customers);
+    Blockbuster.processCommands(movies);
+
+    return 0;
+}
